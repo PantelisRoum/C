ba43b9f17cba7abd5f3161aa2751bbd80a53820b
PantelisRoum <104692010+PantelisRoum@users.noreply.github.com>
2024-02-22 12:12:23 +0200

    Delete sum_dif_prod_Div.cpp

--- sum_dif_prod_Div.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-    int i,j ;
-    printf("Enter 2 integers");
-    scanf("%d%d",&i,&j);
-    printf("sum=%d Diff=%d prod=%d Div=%f\n", i+ j,i - j,i*j,(double)i/j);
-       return 0;
-}
